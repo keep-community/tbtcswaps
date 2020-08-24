73066523565cde70db2293ab7a518cadddb8e708
corollari <corollari@users.noreply.github.com>
2020-08-24 05:54:51 +0000

    deploy: 8510c9f4638bbd62ca1b0ca8c30c49ab124a4f7a

---
+++ precache-manifest.d81fde07ae6b4a3ff5fd4cb5758cffc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf60e39dfc7c0fc43b5b89acbf6601d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ab8113bf6edf348b685",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "72b6cfb03024e524cc78",
+    "url": "/static/js/2.a28a5a48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a28a5a48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ab8113bf6edf348b685",
+    "url": "/static/js/main.1004e3e4.chunk.js"
+  },
+  {
+    "revision": "aa6f0c87fa9ca1c20304",
+    "url": "/static/js/runtime-main.fba19f84.js"
+  }
+]);
