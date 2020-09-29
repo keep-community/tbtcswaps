db536f7dc0e5dadd09e2f16c0c20552d76597bc3
Salah Karim <salahkarimka@gmail.com>
2020-09-29 13:23:28 +0200

    Add error message to content block

--- client/src/views/Widget/common/ContentBlock/index.tsx
+++ client/src/views/Widget/common/ContentBlock/index.tsx
@@ -3,21 +3,25 @@ import { default as TextQR } from "./TextQR";
 
 interface ContentBlockProps {
   label?: string;
+  error?: string
 }
 
 const ContentBlock: React.FC<ContentBlockProps> = (props) => {
-  const { label, children } = props;
+  const { label, children, error } = props;
   return (
     <div className="invoice__block invoice__block--invoice">
       {label && <div className="invoice__block-title">{label}</div>}
       {!children ? (
-        <textarea
-          spellCheck="false"
-          className="invoice__block-content invoice__block-content--editable row"
-        />
+        <>
+          <textarea
+            spellCheck="false"
+            className={`invoice__block-content invoice__block-content--editable row ${error !== undefined ? 'is_error' : ''}`}
+          />
+          {error !== undefined && <span className="form-block__message invoice__block-error" >{error}</span>}
+        </>
       ) : (
-        <div className="invoice__block-content row">{children}</div>
-      )}
+          <div className="invoice__block-content row">{children}</div>
+        )}
     </div>
   );
 };
